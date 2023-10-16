467f268f5551267cf4b227dcefb27a895e9dd1ca
wellvibes <91117889+jgp0@users.noreply.github.com>
2023-10-16 13:04:27 +0100

    #

--- engine/main.py
+++ engine/main.py
@@ -1,50 +1,56 @@
+import os
 import psutil
-import csv
+import socket
 import time
+import csv
+from tabulate import tabulate
 
-# Función para obtener el tráfico de red para cada proceso
-def obtener_trafico_de_red():
-    conexiones = psutil.net_connections(kind='inet')
-    trafico_por_proceso = {}
-
-    for conexion in conexiones:
-        pid = conexion.pid
-        if pid is not None:
-            proceso = psutil.Process(pid)
-            nombre_proceso = proceso.name()
-            if nombre_proceso in trafico_por_proceso:
-                trafico = trafico_por_proceso[nombre_proceso]
-            else:
-                trafico = {'Enviado (bytes)': 0, 'Recibido (bytes)': 0}
-
-            trafico['Enviado (bytes)'] += conexion.sent
-            trafico['Recibido (bytes)'] += conexion.recv
-            trafico_por_proceso[nombre_proceso] = trafico
-
-    return trafico_por_proceso
-
-# Función para guardar los datos en un archivo CSV
-def guardar_en_csv(trafico_por_proceso, ruta_csv):
-    with open(ruta_csv, mode='w', newline='') as archivo_csv:
-        campos = ['Proceso', 'Enviado (bytes)', 'Recibido (bytes)']
-        escritor_csv = csv.DictWriter(archivo_csv, fieldnames=campos)
-        escritor_csv.writeheader()
-
-        for proceso, trafico in trafico_por_proceso.items():
-            escritor_csv.writerow({
-                'Proceso': proceso,
-                'Enviado (bytes)': trafico['Enviado (bytes)'],
-                'Recibido (bytes)': trafico['Recibido (bytes)']
-            })
-
-if __name__ == "__main__":
-    ruta_csv = 'trafico_de_red.csv'
-
+def resolve_domain(ip):
     try:
-        while True:
-            trafico_por_proceso = obtener_trafico_de_red()
-            guardar_en_csv(trafico_por_proceso, ruta_csv)
-            print("Datos guardados en", ruta_csv)
-            time.sleep(5)  # Intervalo de tiempo entre mediciones (5 segundos)
-    except KeyboardInterrupt:
-        print("Monitoreo de red finalizado.")
+        domain = socket.gethostbyaddr(ip)
+        return domain[0]
+    except socket.herror:
+        return "N/A"
+
+def scan_and_save_connections(interval=1):
+    active_connections = set()  # Usamos un conjunto para evitar duplicados
+    log_file = 'data/connections.csv'
+
+    while True:
+        external_connections = []
+
+        for conn in psutil.net_connections(kind='inet'):
+            if conn.raddr and conn.raddr.ip != '127.0.0.1':
+                remote_ip = conn.raddr.ip
+
+                if remote_ip not in active_connections:
+                    active_connections.add(remote_ip)
+
+                    pid = conn.pid or "N/A"
+                    if pid != "N/A":
+                        pid = int(pid)
+                    remote_port = conn.raddr.port
+
+                    # Comprobar si el PID es un entero
+                    if isinstance(pid, int):
+                        process_name = psutil.Process(pid).name()
+                    else:
+                        process_name = "N/A"
+
+                    domain = resolve_domain(remote_ip)
+                    connection_info = [remote_ip, domain, remote_port, process_name]
+                    external_connections.append(connection_info)
+
+        if external_connections:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(tabulate(external_connections, headers=['IP', 'Nombre de dominio', 'Puerto', 'Proceso'], tablefmt='grid'))
+
+            # Guardar las conexiones activas en el archivo CSV de registro
+            with open(log_file, mode='a', newline='') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerows(external_connections)
+
+        time.sleep(interval)
+
+if __name__ == '__main__':
+    scan_and_save_connections()
